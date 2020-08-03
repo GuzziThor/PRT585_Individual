acff2c6dd84c3198a310352f82a24d3856d358c8
guzzithor <augustine.t@icloud.com>
2020-08-03 14:15:55 +0930

    Library

---
+++ MovieTheatre/MovieTheatre/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieTheatre.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
