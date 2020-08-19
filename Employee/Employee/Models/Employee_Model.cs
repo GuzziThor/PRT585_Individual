f3b935c592a27d4f6e5a18112c00b16648e1c374
guzzithor <augustine.t@icloud.com>
2020-08-19 17:10:19 +0930

    DAL added

---
+++ Employee/Employee/Models/Employee_Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.Models
+{
+    public class Employee_Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+
+        public bool IsRetired { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Employee/Employee/_DAL/Services/EmployeeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Employee._DAL.Models;
+using Employee.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee._DAL.Services
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<EmployeeModel> Employees { get; set; }
+
+    }
+}
