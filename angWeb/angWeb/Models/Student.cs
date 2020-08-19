8b6e39379607bc4f2486782c759df80fa152a274
guzzithor <augustine.t@icloud.com>
2020-08-19 13:34:55 +0930

    StudentController

---
+++ angWeb/angWeb/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace angWeb.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public int Roll { get; set; } = 0;
+    }
+}
