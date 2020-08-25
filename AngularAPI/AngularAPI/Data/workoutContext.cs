2f38b976e98dc531522586a2e57c5eb04f658dd9
guzzithor <augustine.t@icloud.com>
2020-08-25 13:07:20 +0930

    started Angular API

---
+++ AngularAPI/AngularAPI/Data/workoutContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AngularAPI.Models;
+
+    public class workoutContext : DbContext
+    {
+        public workoutContext (DbContextOptions<workoutContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Workout> Workout { get; set; }
+    }
---
+++ AngularAPI/AngularAPI/Models/Workout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularAPI.Models
+{
+    public class Workout
+    {
+        public int Id {get; set;}
+        public DateTime Date { get; set; }
+        public int DistanceInMeters { get; set; }
+
+        public long TimeInSeconds { get; set; }
+
+    }
+}
